f02f5fc4b8e9ba8323fe54dcb9b5218081c72f99
Rafael <rsomensi@gmail.com>
2020-05-28 17:14:25 -0300

    commit 2

--- desafio10.6.py
+++ desafio10.6.py
@@ -1,2 +1,11 @@
 #Faça um programa que leia 3 números e
 #mostre qual é o maior e qual é o menor.
+
+n1 = float(input('Typ a number: '))
+n2 = float(input('Typ a number: '))
+n3 = float(input('Typ a number: '))
+
+list = [n1, n2, n3]
+
+print('The greatest number is {}!'.format(max(list)))
+print('The sort number is {}'.format(min(list)))
