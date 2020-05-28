858cbecd0d543563fbf64b194cad8ef4215dd6fb
Rafael <rsomensi@gmail.com>
2020-05-28 17:38:10 -0300

    random drawing

--- desafio10.1.py
+++ desafio10.1.py
@@ -5,9 +5,14 @@
 #O programa deverá escrever na tela se o
 # usuário venceu ou perdeu.
 from random import randint
-
+from time import sleep
 n = randint(0, 5)
-c = int(input('Digite um número entre 0 e 5: '))
+print('-=-' * 20)
+print('Vou pensar em um número entre 0 e 5, tente adivinhar...')
+print('-=-' * 20)
+c = int(input('Adivinhe um número entre 0 e 5: '))
+print('PROCESSANDO...')
+sleep(2)
 print('O número sorteado foi {}'.format(n))
 if n == c:
     print('Você ACERTOU!!!')
