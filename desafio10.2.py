f162f65e63734acead4eee030e71040bd669b0bb
Rafael <rsomensi@gmail.com>
2020-05-28 17:47:32 -0300

    traffic ticket

--- desafio10.2.py
+++ desafio10.2.py
@@ -4,10 +4,14 @@
 #sagem dizendo que ele foi multado.
 #A multa vai custar R$ 7,00 por cada km
 #acima do limite.
-vel = int(input('Velocidade auferida: '))
+vel = float(input('Velocidade auferida: '))
 limit = 80
 multa = (vel - limit)*7
 if vel <= limit:
+    print('-Ok-' * 30)
     print('Boa viagem!!!')
+    print('-Ok-' * 30)
 else:
+    print("-X-" * 30)
     print('VOCÊ FOI MULTADO.\nO valor da sua multa é R$ {:.2f}'.format(multa))
+    print('-X-' * 30)
