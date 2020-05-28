a7efcf1b19e3e32abb63e5f0cdf8d2ad5db2ebe3
Rafael <rsomensi@gmail.com>
2020-05-28 17:04:08 -0300

    commit 2

--- desafio10.8.py
+++ desafio10.8.py
@@ -5,9 +5,9 @@ r1 = int(input('Digite o comprimento da reta 1: '))
 r2 = int(input('Digite o comprimento da reta 2: '))
 r3 = int(input('Digite o comprimento da reta 3: '))
 
-triang = (r1 + r2) >= r3
-triang2 = (r2 + r3) >= r1
-triang3 = (r3 + r1) >= r2
+triang = (r1 + r2) > r3
+triang2 = (r2 + r3) > r1
+triang3 = (r3 + r1) > r2
 
 if triang and triang2 and triang3 is True:
     print('As suas três retas PODEM FORMAR um triângulo!!!')
