380d7f1e3e3ad52ae3a3395ab0d09f3b847c83df
Rafael <rsomensi@gmail.com>
2020-05-29 09:59:07 -0300

    Multa de trânsito com cores

--- desafio10.2.py
+++ desafio10.2.py
@@ -8,10 +8,10 @@ vel = float(input('Velocidade auferida: '))
 limit = 80
 multa = (vel - limit)*7
 if vel <= limit:
-    print('-Ok-' * 30)
-    print('Boa viagem!!!')
-    print('-Ok-' * 30)
+    print('\033[1;32m-Ok-\033[m' * 30)
+    print('\033[1;40;32mBoa viagem!!!\033[m')
+    print('\033[1;32m-Ok-\033[m' * 30)
 else:
-    print("-X-" * 30)
-    print('VOCÊ FOI MULTADO.\nO valor da sua multa é R$ {:.2f}'.format(multa))
-    print('-X-' * 30)
+    print("\033[1;31m-X-\033[m" * 30)
+    print('\033[1;31;40mVOCÊ FOI MULTADO.\033[m\n\033[1;31;40mO valor da sua multa é R$ {:.2f}\033[m'.format(multa))
+    print('\033[1;31m-X-\033[m' * 30)
