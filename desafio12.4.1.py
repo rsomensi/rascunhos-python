828d947382ff5141a2ad0ab85cff5ed02940ca71
Rafael <rsomensi@gmail.com>
2020-06-01 17:19:41 -0300

    Military enlistment

---
+++ desafio12.4.1.py
@@ -0,0 +1,19 @@
+'''#Faça um programa que leia o ano de nascimento
+#de um jovem, de acordo com a sua idade:
+# - se ele ainda vai se alistar ao s.militar.
+#-Se é a hora de se alistar.
+#-Se já passou do tempo do alistamento.
+#Deverá mostrar o tempo que falta ou que passou do prazo.'''
+
+born = int(input('What year were you born: '))
+from datetime import date
+current_year = date.today().year
+current_age = current_year - born
+military_age = 18
+print('We are in {} and you have {} years old!'.format(current_year, current_age))
+if military_age > current_age:
+    print('There still {} years to go before his military enlistment!'.format(military_age - current_age))
+elif military_age < current_age:
+    print('{} years have passed since his military enlistment!'.format(current_age - military_age))
+else:
+    print('Go to a military enlistment center this year!')
