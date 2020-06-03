5f1f5fe28f69ffa6c9342f7423dffaf779d92e04
Rafael <rsomensi@gmail.com>
2020-06-03 16:28:10 -0300

    Triang crit

--- desafio12.7.py
+++ desafio12.7.py
@@ -3,3 +3,21 @@
 #-Equilatero = todos os lados iguais
 #-Isósceles = dois lados iguais
 #-Escaleno = todos os lados diferentes
+r1 = float(input('Digite o comprimento da reta 1: '))
+r2 = float(input('Digite o comprimento da reta 2: '))
+r3 = float(input('Digite o comprimento da reta 3: '))
+
+triang = (r1 + r2) > r3
+triang2 = (r2 + r3) > r1
+triang3 = (r3 + r1) > r2
+
+if triang and triang2 and triang3 is True:
+    print('As suas três retas \033[1;32mPODEM FORMAR\033[m um triângulo!!!')
+    if r1 != r2 != r3 != r1:
+        print('Trata-se de um triângulo ESCALENO.')
+    elif r1 == r2 and r2 != r3 or r1 == r3 and r3 != r2 or r3 == r2 and r2 != r1:
+        print('Trata-se de um triângulo ISÓCELES.')
+    elif r1 == r2 == r3:
+        print('Trata-se de um triângulo EQUILATERO.')
+else:
+    print('As suas retas \033[1;31mNÃO PODEM\033[m formar um triângulo!!!')
