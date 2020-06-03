45a43a8de0f9a322836f2f8c7d064f6f616a943d
Rafael <rsomensi@gmail.com>
2020-06-03 16:10:33 -0300

    Category by age

--- desafio12.6.py
+++ desafio12.6.py
@@ -9,13 +9,14 @@ y_born = int(input('Year you were born: '))
 from datetime import date
 current_y = date.today().year
 current_age = current_y - y_born
+print('You have {} years old.'.format(current_age))
 if current_age <= 9:
     print('Child category 1.')
-elif current_age > 9 and current_age <= 14:
+elif current_age <= 14:
     print('Child category 2.')
-elif current_age > 14 and current_age <= 19:
+elif current_age <= 19:
     print('Junior category.')
-elif current_age > 19 and current_age <= 20:
+elif current_age <= 25:
     print('Senior category.')
 else:
     print('Master category.')
