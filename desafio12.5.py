70521b915327af95c3fdc63a94f81f7b7064b748
Rafael <rsomensi@gmail.com>
2020-06-03 09:57:27 -0300

    School average

--- desafio12.5.py
+++ desafio12.5.py
@@ -9,7 +9,7 @@ t2 = float(input('Insert the second score: '))
 school_average = (t1 + t2)/2
 if school_average < 5.0:
     print('Your average is {}, have failed!'.format(school_average))
-elif school_average > 5.0 and school_average < 7.0:
+elif school_average >= 5.0 and school_average < 7.0:
     print('Your average is {}, you are recovering from school!'.format(school_average))
 else:
     print('Congratulations, your average is {}, you have been approved!!!'.format(school_average))
